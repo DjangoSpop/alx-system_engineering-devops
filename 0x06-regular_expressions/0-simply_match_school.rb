fa6ed50dd27e320b18c6e1b700aa5bd423950fe3
DjangoBeho <behodjango@gmail.com>
2023-11-01 00:59:02 +0200

    do that we do

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-
-# Define the regular expression
-pattern = /School/
-
-# Get the argument from the command line
-input_string = ARGV[0]
-
-# Find all matches using the scan method
-matches = input_string.scan(pattern)
-
-# Print the matches
-puts matches.join
-
+puts ARGV[0].scan(/School/).join
