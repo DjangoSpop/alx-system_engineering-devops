6528601991360704f0deef3173caf6c60e6c3529
DjangoSpop <behodjango@gmail.com>
2023-12-24 05:40:53 -0800

    we cand dot this

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,17 @@
 # site.pp
 
+exec { 'start_killmenow_process':
+  command     => '/path/to/your/start_killmenow_script', # Replace this with the actual command to start your process
+  path        => ['/bin', '/usr/bin'],
+  onlyif      => 'pgrep -f "killmenow"',
+  refreshonly => true,
+}
+
 exec { 'killmenow_process':
   command     => 'pkill -f "killmenow"',
   path        => ['/bin', '/usr/bin'],
   onlyif      => 'pgrep -f "killmenow"',
   refreshonly => true,
+  subscribe   => Exec['start_killmenow_process'],
 }
 
