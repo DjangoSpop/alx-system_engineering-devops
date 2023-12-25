992de1b0577340d570b3a93b04bd631313f5abb9
DjangoSpop <behodjango@gmail.com>
2023-12-25 06:03:45 -0800

    we do this

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,7 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package { 'python3-pip':
-  ensure => present,
-}
-
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
 
