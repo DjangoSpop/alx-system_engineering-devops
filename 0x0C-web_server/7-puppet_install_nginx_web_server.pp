7a7260bf4d391d75d4e261dc86df5c95a7a635aa
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-01-04 22:31:52 +0200

    WE

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,48 @@
-# Automating project requirements using Puppet
-
 package { 'nginx':
-  ensure => installed,
+ ensure => installed,
 }
 
 file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me http:https://github.com/DjangoSpop/alx-system_engineering-devops;',
+ ensure => 'present',
+ path   => '/etc/nginx/sites-enabled/default',
+ after => 'listen 80 default_server;',
+ line   => 'rewrite ^/redirect_me http:https://github.com/DjangoSpop/alx-system_engineering-devops;',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World!',
+ content => 'Hello World!',
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+file { '/var/www/html/404.html':
+ content => 'Error 404: Page not found!',
 }
+ service { 'nginx':
+ ensure => running,
+ require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-enabled/default':
+ ensure => file,
+ notify => Service['nginx'],
+ content => "
+ server {
+     listen 80 default_server;
+     listen [::]:80 default_server;
+
+     root /var/www/html;
+     index index.html index.htm index.nginx-debian.html;
+
+     server_name _;
+
+     location / {
+       try_files $uri $uri/ =404;
+     }
+
+     error_page 404 /404.html;
+     location = /404.html {
+       internal;
+     }
+ }
+ ",
+}
+
