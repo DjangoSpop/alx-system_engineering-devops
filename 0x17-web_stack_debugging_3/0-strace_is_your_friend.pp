9b66baa836a19031eb7c553e7be6a0dc253b160d
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-02-17 09:57:11 +0200

    we can

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,27 +1,6 @@
-# FILEPATH: /c:/Users/ahmed el bahi/alx-system_engineering-devops/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Install Apache package
-package { 'apache2':
-  ensure => installed,
-}
-
-# Configure Apache
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  content => '
-    <VirtualHost *:80>
-      ServerAdmin webmaster@localhost
-      DocumentRoot /var/www/html
-      ErrorLog ${APACHE_LOG_DIR}/error.log
-      CustomLog ${APACHE_LOG_DIR}/access.log combined
-    </VirtualHost>
-  ',
-  require => Package['apache2'],
-}
-
-# Restart Apache service
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/apache2/sites-available/000-default.conf'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
